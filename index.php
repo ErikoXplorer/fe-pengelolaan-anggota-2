0df278dcc8af68a521d56bf0e63176b772065e0a
ErikoXplorer <erkaggra155@gmail.com>
2024-10-17 13:31:11 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ipsum tempora nostrum eos ipsa voluptas rem corrupti quasi modi ad praesentium assumenda facere, natus, tempore tenetur voluptate mollitia cumque recusandae.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae modi, maxime molestiae incidunt velit ipsa delectus repellendus expedita. Iste rem accusamus modi similique velit quas quod corporis dolores. Voluptatum tempore expedita, debitis ipsum reiciendis consequatur, at perspiciatis illo repellendus rerum, molestias eos! Ut alias aliquam itaque reiciendis provident temporibus hic enim dolore porro deleniti laboriosam, obcaecati exercitationem ratione eos accusantium corrupti saepe. Quae ex quam exercitationem tempore! Quaerat iusto necessitatibus voluptatibus veritatis molestias minus dolore illo vel ipsam quibusdam! Quasi consequatur temporibus a, impedit illo, ducimus voluptas quae excepturi modi dolorem veniam beatae placeat? Eos molestias ipsa aut eveniet consequuntur.</p>
+    <ul>
+        <li>List</li>
+    </ul>
 </body>
 </html>
